6a21e83c4c4bfacbb2afce54778c81f23c1765bb
ADMIN <351672318@qq.com>
2018-12-25 17:48:25 +0800

    添加菜单功能

---
+++ HzyAdmin/Areas/Admin/Controllers/Base/ExecutivesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BLL;
+using DbFrame.Class;
+using Common;
+using System.Collections;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using global::Models;
+using Microsoft.AspNetCore.Http;
+
+namespace HzyAdmin.Areas.Admin.Controllers.Base
+{
+    public class ExecutivesController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- HzyAdmin/Areas/Admin/Controllers/Base/MemberController.cs
+++ HzyAdmin/Areas/Admin/Controllers/Base/MemberController.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using BLL;
+using DbFrame.Class;
+using Common;
+using System.Collections;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using global::Models;
+using Microsoft.AspNetCore.Http;
 
 namespace HzyAdmin.Areas.Admin.Controllers.Base
 {
     //
-    using BLL;
-    using DbFrame.Class;
-    using Common;
-    using System.Collections;
-    using Microsoft.AspNetCore.Hosting;
-    using System.IO;
-    using global::Models;
-    using Microsoft.AspNetCore.Http;
+
 
     /// <summary>
     /// 会员管理
